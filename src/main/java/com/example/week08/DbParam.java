ea6a1616b7ac0724704873ff6483cd179cb4a670
ltx <ltx0720@gmail.com>
2022-04-24 22:29:30 +0800

    week08

---
+++ src/main/java/com/example/week08/DbParam.java
@@ -0,0 +1,43 @@
+package com.example.week08;
+
+public class DbParam {
+
+    private String userId;
+
+    private String databaseName;
+
+    private String tableName;
+
+    public DbParam(String userId, String databaseName, String tableName) {
+        this.userId = userId;
+        this.databaseName = databaseName;
+        this.tableName = tableName;
+    }
+
+    public DbParam() {
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
---
+++ src/main/java/com/example/week08/RoutingAspect.java
@@ -0,0 +1,54 @@
+package com.example.week08;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Method;
+
+import static com.example.week07.datasource.DynamicDataSource.DATABASE_1;
+import static com.example.week07.datasource.DynamicDataSource.DATABASE_2;
+
+@Aspect
+@Service
+public class RoutingAspect {
+
+    @Pointcut("@annotation(SimpleRouting)")
+    public void pointCut() {
+    }
+
+
+    @Before("pointCut()")
+    public void around(JoinPoint joinPoint) {
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        SimpleRouting annotation = method.getAnnotation(SimpleRouting.class);
+        Object[] args = joinPoint.getArgs();
+        DbParam dbParam;
+        if (args.length != 0 && args[0] instanceof DbParam) {
+            dbParam = (DbParam) args[0];
+        } else {
+            throw new RuntimeException("arg error");
+        }
+
+        // 按 userId 路由
+        if (StringUtils.isEmpty(annotation.dataBase())) {
+            dbParam.setDatabaseName(getDataBaseName(dbParam.getUserId()));
+        }
+
+        dbParam.setTableName(getTableName(annotation.table(), dbParam.getUserId()));
+    }
+
+    private String getDataBaseName(String userId) {
+        return (userId.hashCode() & 1) == 0 ? DATABASE_1 : DATABASE_2 ;
+    }
+
+    private String getTableName(String tableName, String userId) {
+        int index = userId.hashCode() & 15;
+        return index < 10 ? tableName + "0" + index : tableName +index;
+    }
+
+}
